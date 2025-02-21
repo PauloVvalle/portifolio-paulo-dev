b6aca3583db48016d470cb5ea224b2189b27084e
PauloVvalle <paulo1111555@outlook.com>
2025-02-21 13:07:06 -0300

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
-        <div className='p-12  flex justify-between'>
+        <div className='p-12 grid grid-cols-2 place-content-center md:flex md:justify-between'>
             <Image
               src="/images/logo-foot.png"
               alt="Logo"
